639db3a89c4b2ebb772aca7f7beb13572cb4b51d
Uwe Dauernheim <uwe@klarna.com>
2011-08-22 21:50:42 +0200

    Use any key to stop animation

--- src/nyancat.c
+++ src/nyancat.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <termios.h>  // kbhit
+#include <sys/time.h> //
+#include <string.h>   //
+#include <unistd.h>   //
 
 static const int   WIDTH=60;
 static const int   ANGLE=4;
@@ -18,13 +22,31 @@ static const char* CAT[] = {
   "  UU  UU "
 };
 
-int main(void) {
-  int i=0, x, y, t, loop=1;
+int _kbhit() {
+  int r;
+  fd_set read_fd;
+  struct timeval tv = {0};
+  struct termios term, oterm;
+
+  tcgetattr(STDIN_FILENO, &oterm);
+  memcpy(&term, &oterm, sizeof(term));
+  cfmakeraw(&term);
+  tcsetattr(STDIN_FILENO, TCSANOW, &term);
+  FD_ZERO(&read_fd);
+  FD_SET(STDIN_FILENO, &read_fd);
+  r = select(1, &read_fd, NULL, NULL, &tv);
+  tcsetattr(STDIN_FILENO, TCSANOW, &oterm);
+
+  return r;
+}
+
+int main() {
+  int i=0, x, y, t;
   const int COLOURS_LEN = sizeof(COLOURS) / sizeof(int);
   const int FLAG_LEN    = sizeof(FLAG)    / sizeof(char) - 1;
   printf("%d\n", FLAG_LEN);
   
-  while (loop) {
+  while (1) {
     for (y = 0; y < COLOURS_LEN; y++) { // line loop
       printf("\x1b[1;%dm", COLOURS[y]); // set rainbow line colour
       for (x = 0; x < WIDTH - ANGLE*(COLOURS_LEN-y); x++) // rainbow line
@@ -35,6 +57,9 @@ int main(void) {
       printf("\x1b[1m"); // set bright color for cat
       puts(CAT[y%COLOURS_LEN + (i%10<COLOURS_LEN ? 0 : COLOURS_LEN)]); // cat
     }
+    
+    if (_kbhit()) // key pressed?
+      break;
     
     i++;
     usleep(DELAY); // wait x ms
